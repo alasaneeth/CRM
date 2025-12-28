efd1329e5b9ab7888a25d5c2d2a9eda5e53decb3
alasaneeth <alasaneeth22@gmail.com>
2025-12-29 01:15:24 +0400

    Add: Create ApplicationDbContext

---
+++ CRM.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRM.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
