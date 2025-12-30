82de4f7289f976be491615d163bedd6b2cb67402
alasaneeth <alasaneeth22@gmail.com>
2025-12-30 21:26:44 +0400

    Feate: Category create function

---
+++ CRM.Web/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using CRM.Web.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPost]
+        public ActionResult Create([FromBody] Models.Category category)
+        {
+
+            if(!ModelState.IsValid)
+            {
+                 return BadRequest(ModelState);
+            }
+            _dbContext.Category.Add(category);
+            _dbContext.SaveChanges();
+            return Ok(category);
+        }
+    }
+}
